e5df9134b952ba76d2f4966ab2e6a1ca08abb77b
Ravi Kiran <ravi.kiran8844@gmail.com>
2024-05-15 16:56:55 +0530

    updated api link

--- src/app/api/weight-loss/route.js
+++ src/app/api/weight-loss/route.js
@@ -1,5 +1,5 @@
 export async function GET(req) {
-    const res = await fetch('https://survey-aol7o6mvqa-el.a.run.app/problem/fa57f076-26a9-4850-add5-204862fe59ba/questions',{
+    const res = await fetch('https://survey-aol7o6mvqa-el.a.run.app/problem/8198fba8-789a-437b-a302-cefe4553886e/questions',{
         headers: {
             'Content-Type': 'application/json',
           }
